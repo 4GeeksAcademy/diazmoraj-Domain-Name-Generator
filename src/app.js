6ff7fb8119bf2375b8b928afc55756daf33c254e
diazmoraj <103903062+diazmoraj@users.noreply.github.com>
2024-05-11 04:55:51 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".us"];
+
+  function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  function getDomain(domain) {
+    let max = domain.length;
+    let min = 0;
+    let random = getRandom(min, max);
+    return domain[random];
+  }
+
   console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML =
+    getDomain(pronoun) + getDomain(adj) + getDomain(noun) + getDomain(domain);
 };
